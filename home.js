9a8649210c40a905dd440cf5eb773ba0f0440328
nisha-2408 <87900886+nisha-2408@users.noreply.github.com>
2021-12-23 19:45:05 +0530

    intial home and login

--- home.js
+++ home.js
@@ -11,11 +11,18 @@ function openSide() {
 function login(){
     var element = document.getElementById("log1");
     element.classList.remove("w3-animate-right");
-    document.getElementById("log1").style.display = "block";
+    document.getElementById("log1").style.display = "flex";
     document.getElementById("log2").style.display = "none";
 
 }
 function sign(){
-    document.getElementById("log2").style.display = "block";
+    document.getElementById("log2").style.display = "flex";
     document.getElementById("log1").style.display = "none";
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
