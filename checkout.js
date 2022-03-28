8bcff312ab1e107f3a2abd470442b3f868e6d9af
nisha-2408 <87900886+nisha-2408@users.noreply.github.com>
2022-03-28 16:37:14 +0530

    update

--- checkout.js
+++ checkout.js
@@ -6,6 +6,19 @@ function off() {
   document.getElementById("overlay").style.display = "none";
 }
 
+function onn() {
+  document.getElementById("over").style.display = "block";
+}
+
+function of() {
+  document.getElementById("over").style.display = "none";
+}
+function get() {
+  var get;
+  get = document.getElementById("pinn").value;
+  console.log(get);
+}
+
 const inputs = document.getElementById("addr").querySelectorAll("input");
 
 const patterns = {
@@ -55,3 +68,32 @@ function ac(i){
     else
       shad[n].classList.remove("shad-act");
 }
+var value = [];
+function submi()
+{
+  var i=0;
+  for(var n=0;n<inputs.length;n++)
+  {
+    if(inputs[n].classList.contains("invalid"))
+    {
+      alert(inputs[n].value+" is not valid");
+      i=1;
+      break;
+    }
+    else{
+      value[n] = inputs[n].value;
+      continue;
+    }
+  }
+  if(i==0)
+  {
+    alert("Added successfully");
+    off();
+    for(var n=0;n<inputs.length;n++)
+    {
+      inputs[n].value="";
+    }
+  }
+}
+
+console.log(value);
